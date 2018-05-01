0179465a49814d3bbde14254b29f4035a390137f
Billie Thompson <billie@billiecodes.com>
2018-05-01 23:28:34 +0100

    Quick coding Kata to keep my rust skills up

--- README.md
+++ README.md
@@ -1,16 +1,48 @@
-# Kata: Count the "the"s
+# Kata: Karate Chop
 
 ## Challenge
 
-Count the "the" from stdin without an "if" or a loop.
+[Karate Chop](http://codekata.com/kata/kata02-karate-chop/) Kata from [codekata](http://codekata.com/kata/kata02-karate-chop/)
+
+     
+
+Specification
+
+> Write a binary chop method that takes an integer search target and a sorted array of integers. It should return the integer index of the target in the array, or -1 if the target is not in the array. The signature will logically be:
+>
+> ```ruby
+> chop(int, array_of_int)  -> int
+> ```
+> 
+> You can assume that the array has less than 100,000 elements. For the purposes of this Kata, time and memory performance are not issues (assuming the chop terminates before you get bored and kill it, and that you have enough RAM to run it).
 
 ## Test Data
 
- * "hello to the world" => 1
- * "the quick brown fox jumps over the lazy dog" => 2
- * "The quick brown fox jumps over the lazy dog" => 2
- * "Their name is The." => 2
- * "Their name is The\0" => 1
+```ruby
+def test_chop
+  assert_equal(-1, chop(3, []))
+  assert_equal(-1, chop(3, [1]))
+  assert_equal(0,  chop(1, [1]))
+  #
+  assert_equal(0,  chop(1, [1, 3, 5]))
+  assert_equal(1,  chop(3, [1, 3, 5]))
+  assert_equal(2,  chop(5, [1, 3, 5]))
+  assert_equal(-1, chop(0, [1, 3, 5]))
+  assert_equal(-1, chop(2, [1, 3, 5]))
+  assert_equal(-1, chop(4, [1, 3, 5]))
+  assert_equal(-1, chop(6, [1, 3, 5]))
+  #
+  assert_equal(0,  chop(1, [1, 3, 5, 7]))
+  assert_equal(1,  chop(3, [1, 3, 5, 7]))
+  assert_equal(2,  chop(5, [1, 3, 5, 7]))
+  assert_equal(3,  chop(7, [1, 3, 5, 7]))
+  assert_equal(-1, chop(0, [1, 3, 5, 7]))
+  assert_equal(-1, chop(2, [1, 3, 5, 7]))
+  assert_equal(-1, chop(4, [1, 3, 5, 7]))
+  assert_equal(-1, chop(6, [1, 3, 5, 7]))
+  assert_equal(-1, chop(8, [1, 3, 5, 7]))
+end
+```
  
 # Running
 
@@ -23,40 +55,61 @@ rustup run nightly cargo fmt --all --
 Run tests
 
 ```
-$ rustup run stable cargo test
-   Compiling kata-count-number-of-the v0.1.0 (file:///tmp/kata-count-number-of-the)
-    Finished dev [unoptimized + debuginfo] target(s) in 2.49 secs
-     Running target/debug/deps/countthe-fe3999f5b5c72633
+$  rustup run stable cargo test             master ✚ ✱
+     Compiling kata-karate-chop v0.1.0 (file:///Users/billie/Documents/Code/kata-karate-chop)
+      Finished dev [unoptimized + debuginfo] target(s) in 1.23 secs
+       Running target/debug/deps/katakaratechop-635aa9985c5a2d1d
+  
+  running 40 tests
+  test tests::case_iterative_01 ... ok
+  test tests::case_iterative_02 ... ok
+  test tests::case_iterative_03 ... ok
+  test tests::case_iterative_04 ... ok
+  test tests::case_iterative_05 ... ok
+  test tests::case_iterative_06 ... ok
+  test tests::case_iterative_07 ... ok
+  test tests::case_iterative_08 ... ok
+  test tests::case_iterative_09 ... ok
+  test tests::case_iterative_10 ... ok
+  test tests::case_iterative_11 ... ok
+  test tests::case_iterative_12 ... ok
+  test tests::case_iterative_13 ... ok
+  test tests::case_iterative_14 ... ok
+  test tests::case_iterative_15 ... ok
+  test tests::case_iterative_16 ... ok
+  test tests::case_iterative_17 ... ok
+  test tests::case_iterative_18 ... ok
+  test tests::case_iterative_19 ... ok
+  test tests::case_iterative_20 ... ok
+  test tests::case_stack_01 ... ok
+  test tests::case_stack_02 ... ok
+  test tests::case_stack_03 ... ok
+  test tests::case_stack_04 ... ok
+  test tests::case_stack_05 ... ok
+  test tests::case_stack_06 ... ok
+  test tests::case_stack_07 ... ok
+  test tests::case_stack_08 ... ok
+  test tests::case_stack_09 ... ok
+  test tests::case_stack_10 ... ok
+  test tests::case_stack_11 ... ok
+  test tests::case_stack_12 ... ok
+  test tests::case_stack_13 ... ok
+  test tests::case_stack_14 ... ok
+  test tests::case_stack_15 ... ok
+  test tests::case_stack_16 ... ok
+  test tests::case_stack_17 ... ok
+  test tests::case_stack_18 ... ok
+  test tests::case_stack_19 ... ok
+  test tests::case_stack_20 ... ok
+  
+  test result: ok. 40 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
+  
+     Doc-tests katakaratechop
+  
+  running 0 tests
+  
+  test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
+ 
 
-running 5 tests
-test tests::it_counts_ignores_capitalisation ... ok
-test tests::it_counts_the_multiple_the ... ok
-test tests::it_ignores_punctuation ... ok
-test tests::it_null_characters_at_the_end_of_string_are_stripped ... ok
-test tests::it_the_single_the ... ok
 
-test result: ok. 5 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
-
-     Running target/debug/deps/countthe-e8ab169716f0301f
-
-running 0 tests
-
-test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
-
-   Doc-tests countthe
-
-running 0 tests
-
-test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
-
-
-```
-
-Run the app
-
-```
-echo "Their name is The" | rustup run stable cargo run
-    Finished dev [unoptimized + debuginfo] target(s) in 0.0 secs
-     Running `target/debug/countthe`
-1
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,261 @@
-extern crate regex;
+pub fn chop_iterative(search_target: i64, search_space: Vec<i64>) -> i64 {
+    let mut current_search_space = search_space;
+    let mut offset = 0 as i64;
 
-use regex::Regex;
-use std::io::BufRead;
+    while current_search_space.len() > 0 {
+        let midpoint = (current_search_space.len() / 2) as i64;
 
-pub fn count_the(reader: &mut BufRead) -> usize {
-    let re = Regex::new("[^a-z0-9]").unwrap();
+        if current_search_space.len() <= 2 {
+            match current_search_space
+                .iter()
+                .position(|&r| r == search_target)
+            {
+                Some(index) => return index as i64 + offset,
+                None => return -1,
+            }
+        }
 
-    reader
-        .split(' ' as u8)
-        .map(|s| -> String { String::from_utf8(s.unwrap()).unwrap().to_lowercase() })
-        .map(|s| re.replace_all(s.as_str(), "").to_string())
-        .filter(|s| s == "the")
-        .count()
+        if current_search_space[midpoint as usize] == search_target {
+            return midpoint + offset;
+        } else if current_search_space[midpoint as usize] > search_target {
+            current_search_space = current_search_space[..midpoint as usize].to_vec();
+        } else if current_search_space[midpoint as usize] < search_target {
+            current_search_space = current_search_space[midpoint as usize..].to_vec();
+            offset += midpoint
+        }
+    }
+
+    -1
+}
+
+pub fn chop_stack(search_target: i64, search_space: Vec<i64>) -> i64 {
+    let midpoint = (search_space.len() / 2) as i64;
+
+    if search_space.len() <= 2 {
+        match search_space.iter().position(|&r| r == search_target) {
+            Some(index) => return index as i64,
+            None => return -1,
+        }
+    }
+
+    if search_space[midpoint as usize] == search_target {
+        return midpoint;
+    } else if search_space[midpoint as usize] > search_target {
+        return chop_stack(search_target, search_space[..midpoint as usize].to_vec());
+    } else if search_space[midpoint as usize] < search_target {
+        let search_result = chop_stack(search_target, search_space[midpoint as usize..].to_vec());
+
+        match search_result {
+            -1 => return -1,
+            _ => return midpoint + search_result,
+        }
+    }
+
+    -1
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::BufReader;
-    use count_the;
+    use chop_iterative;
+    use chop_stack;
 
     #[test]
-    fn it_the_single_the() {
-        let input_data = "hello to the world";
-        let mut reader = BufReader::new(input_data.as_bytes());
-
-        assert_eq!(count_the(&mut reader), 1);
+    fn case_iterative_01() {
+        assert_eq!(-1, chop_iterative(3, vec![]))
     }
 
     #[test]
-    fn it_counts_the_multiple_the() {
-        let input_data = "the quick brown fox jumps over the lazy dog";
-        let mut reader = BufReader::new(input_data.as_bytes());
-
-        assert_eq!(count_the(&mut reader), 2);
+    fn case_iterative_02() {
+        assert_eq!(-1, chop_iterative(3, vec![]))
     }
 
     #[test]
-    fn it_counts_ignores_capitalisation() {
-        let input_data = "The quick brown fox jumps over the lazy dog";
-        let mut reader = BufReader::new(input_data.as_bytes());
-
-        assert_eq!(count_the(&mut reader), 2);
+    fn case_iterative_03() {
+        assert_eq!(-1, chop_iterative(3, vec![1]))
     }
 
     #[test]
-    fn it_ignores_punctuation() {
-        let input_data = "Their name is The.";
-        let mut reader = BufReader::new(input_data.as_bytes());
-
-        assert_eq!(count_the(&mut reader), 1);
+    fn case_iterative_04() {
+        assert_eq!(0, chop_iterative(1, vec![1]))
     }
 
     #[test]
-    fn it_null_characters_at_the_end_of_string_are_stripped() {
-        let input_data = "Their name is The\0";
-        let mut reader = BufReader::new(input_data.as_bytes());
+    fn case_iterative_05() {
+        assert_eq!(0, chop_iterative(1, vec![1, 3, 5]))
+    }
 
-        assert_eq!(count_the(&mut reader), 1);
+    #[test]
+    fn case_iterative_06() {
+        assert_eq!(1, chop_iterative(3, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_iterative_07() {
+        assert_eq!(2, chop_iterative(5, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_iterative_08() {
+        assert_eq!(-1, chop_iterative(0, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_iterative_09() {
+        assert_eq!(-1, chop_iterative(2, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_iterative_10() {
+        assert_eq!(-1, chop_iterative(4, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_iterative_11() {
+        assert_eq!(-1, chop_iterative(6, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_iterative_12() {
+        assert_eq!(0, chop_iterative(1, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_13() {
+        assert_eq!(1, chop_iterative(3, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_14() {
+        assert_eq!(2, chop_iterative(5, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_15() {
+        assert_eq!(3, chop_iterative(7, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_16() {
+        assert_eq!(-1, chop_iterative(0, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_17() {
+        assert_eq!(-1, chop_iterative(2, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_18() {
+        assert_eq!(-1, chop_iterative(4, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_19() {
+        assert_eq!(-1, chop_iterative(6, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_iterative_20() {
+        assert_eq!(-1, chop_iterative(8, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_01() {
+        assert_eq!(-1, chop_stack(3, vec![]))
+    }
+
+    #[test]
+    fn case_stack_02() {
+        assert_eq!(-1, chop_stack(3, vec![]))
+    }
+
+    #[test]
+    fn case_stack_03() {
+        assert_eq!(-1, chop_stack(3, vec![1]))
+    }
+
+    #[test]
+    fn case_stack_04() {
+        assert_eq!(0, chop_stack(1, vec![1]))
+    }
+
+    #[test]
+    fn case_stack_05() {
+        assert_eq!(0, chop_stack(1, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_06() {
+        assert_eq!(1, chop_stack(3, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_07() {
+        assert_eq!(2, chop_stack(5, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_08() {
+        assert_eq!(-1, chop_stack(0, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_09() {
+        assert_eq!(-1, chop_stack(2, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_10() {
+        assert_eq!(-1, chop_stack(4, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_11() {
+        assert_eq!(-1, chop_stack(6, vec![1, 3, 5]))
+    }
+
+    #[test]
+    fn case_stack_12() {
+        assert_eq!(0, chop_stack(1, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_13() {
+        assert_eq!(1, chop_stack(3, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_14() {
+        assert_eq!(2, chop_stack(5, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_15() {
+        assert_eq!(3, chop_stack(7, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_16() {
+        assert_eq!(-1, chop_stack(0, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_17() {
+        assert_eq!(-1, chop_stack(2, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_18() {
+        assert_eq!(-1, chop_stack(4, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_19() {
+        assert_eq!(-1, chop_stack(6, vec![1, 3, 5, 7]))
+    }
+
+    #[test]
+    fn case_stack_20() {
+        assert_eq!(-1, chop_stack(8, vec![1, 3, 5, 7]))
     }
 }
